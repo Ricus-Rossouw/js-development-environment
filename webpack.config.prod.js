2828d146cd16533f850fa09d46ace6124b65c777
Ricus Rossouw <ricochet@pool-100-1-0-18.nwrknj.fios.verizon.net>
2018-05-08 09:40:58 +0200

    bundle splitting and minification

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,8 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
   debug: true,
@@ -14,11 +16,16 @@ export default {
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: '[name].[chunkhash].js'
   },
   plugins: [
+    // Generate an external css file with a hash in the filename
+    new ExtractTextPlugin('[name].[contenthash].css'),
+    // Hash the files using MD5 to change the name when content changes
+    new WebpackMd5Hash(),
+
     // Create separate cached bundles of vendor libraries
-    new webpack.optimize.CommonChunkPlugin({
+    new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor'
     }),
 
@@ -49,7 +56,7 @@ export default {
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loaders: ['style', 'css']},
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')},
     ]
   }
 }
